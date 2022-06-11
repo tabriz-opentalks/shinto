7223711cf122cf96760fd0861d34c04ecad4a230
Mohammadreza <moharnadreza@gmail.com>
2022-06-11 16:44:33 +0430

    feat: add input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,38 @@
+import Wrapper from './Wrapper';
+import type { TextInputProps } from './types';
+import classNames from 'classnames';
+
+const TextInput = ({
+  label,
+  name,
+  disabled = false,
+  className = '',
+  wrapperClassName = '',
+  required,
+  ...rest
+}: TextInputProps): JSX.Element => (
+  <Wrapper
+    name={name}
+    label={label}
+    disabled={disabled}
+    required={required}
+    wrapperClassName={wrapperClassName}
+  >
+    <input
+      id={name}
+      name={name}
+      disabled={disabled}
+      className={classNames(
+        'w-full py-2 px-6 text-gray-800 placeholder-gray-400 rounded-xl border-2 border-gray-300',
+        'transition-colors duration-100 ease-out',
+        'disabled:pointer-events-none disabled:bg-gray-100',
+        'focus:outline-none focus:border-gray-800',
+        className
+      )}
+      aria-describedby={`${name}-description`}
+      {...rest}
+    />
+  </Wrapper>
+);
+
+export default TextInput;
