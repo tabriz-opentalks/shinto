871a8a768c899595409719f5c850c04d8b581aa3
Mohammadreza <moharnadreza@gmail.com>
2022-06-21 12:11:35 +0430

    style: update default font for `tailwindcss`

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
+    fontFamily: {
+      sans: ['Sahel', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
